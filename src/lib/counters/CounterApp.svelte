6368b0c4fcc6be3dca1ac1a831e8db22c1cd6b34
klezovich <arthur@trevor.io>
2023-05-28 11:23:36 +0200

    Remove unused imports

--- src/lib/counters/CounterApp.svelte
+++ src/lib/counters/CounterApp.svelte
@@ -1,6 +1,4 @@
 <script>
-    import Counters from "./Counters.svelte";
-    import CounterSummary from "./CounterSummary.svelte";
     import CounterSummaryContainer from "./CounterSummaryContainer.svelte";
     import CountersContainer from "./CountersContainer.svelte";
 </script>
