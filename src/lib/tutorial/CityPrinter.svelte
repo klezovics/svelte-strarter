450ce32bdf9e5b5069ddcc219cf5ddb6477ac281
klezovich <arthur@trevor.io>
2023-05-28 14:47:49 +0200

    Add code

---
+++ src/lib/tutorial/CityPrinter.svelte
@@ -0,0 +1,15 @@
+<script>
+    let cities = [
+        {id:1, name: 'London'},
+        {id:2, name: 'Paris'},
+        {id:3, name: 'New York'},
+    ]
+</script>
+
+<h1>This is a city list</h1>
+<ul>
+<!--    Feature -> Can iterate over array when rendering. Also can get index-->
+    {#each cities as city,index (city.id)}
+        <li>{index} - {city.name}</li>
+    {/each}
+</ul>
---
+++ src/lib/tutorial/MyApiConsumer.svelte
@@ -0,0 +1,25 @@
+<script>
+
+    let character = null;
+
+    async function getRandomStarWarsCharacter() {
+        const randomNumber = Math.floor(Math.random() * 10) + 1;
+        const apiResponse = await fetch(
+            `https://swapi.dev/api/people/${randomNumber}/`
+        );
+
+        return await apiResponse.json();
+    }
+
+    let promise = getRandomStarWarsCharacter();
+</script>
+
+
+<h1>This is character info!</h1>
+
+<!--Feature: Can use await to wait for a promise to resolve-->
+{#await promise}
+    <h1>Loading ...</h1>
+{:then character}
+    <h1>{character.name}</h1>
+{/await}
